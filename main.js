a30ec27ed96866e785a4a20cf53078f0af004575
Gabriel Batista <gabriel.batista.7@hotmail.com>
2024-02-21 01:20:51 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function copyPageContent() {
+    var pageText = "";
+    var formElements = document.querySelectorAll("form textarea");
+    
+    formElements.forEach(function(element) {
+        pageText += element.previousElementSibling.innerText + "\n";
+        pageText += element.value + "\n\n";
+    });
+
+    var copyText = document.createElement("textarea");
+    copyText.value = pageText;
+    document.body.appendChild(copyText);
+    copyText.select();
+    document.execCommand("copy");
+    document.body.removeChild(copyText);
+    alert('Conteúdo da página copiado com sucesso!');
+}
